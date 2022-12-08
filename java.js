c892325c59250e092f011f24b8c92da1e0f76146
marcocassani <marco.cassani@menloschool.org>
2022-12-07 21:14:16 -0800

    car_final_code

---
+++ java.js
@@ -0,0 +1,32 @@
+const scrollOffset = 100;
+ 
+const scrollElement = document.querySelector(".js-scroll");
+ 
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+ 
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+ 
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+ 
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+ 
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
